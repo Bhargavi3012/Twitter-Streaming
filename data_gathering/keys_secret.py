4ec4f9bf2bb94b60d627709eaf9a2326f2e17190
Bhargavi3012 <bhargavijoshi1997@gmail.com>
2020-09-26 13:43:31 +0530

    Initial Commit

---
+++ data_gathering/keys_secret.py
@@ -0,0 +1,4 @@
+consumer_key = 'ehcadUlQExt8D80cIfKd90dEc'
+consumer_secret = 'ai7ub8Sn8KfXSOkTWU8Xzedh0lL029tYyp8ZzXY1VBX0PanizA'
+access_token = '1248646265409134592-NMwwkuJ2VoDt4Q6DTI3RuxVrwDmox3'
+access_token_secret = 'TzTkJrwvyraBBmESRF7OTZ60eOziqTfs7h5GfF6eKiEG7'
---
+++ data_gathering/streaming.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+# import packages
+from tweepy import OAuthHandler
+from tweepy import API
+from tweepy import Stream
+from sqlalchemy import create_engine
+from sqlalchemy_utils import database_exists, create_database
+from urllib3.exceptions import ProtocolError
+from slistener import SListener
+from keys_secret import consumer_key, consumer_secret
+from keys_secret import access_token, access_token_secret
+
+
+# In[ ]:
+
+
+# consumer key authentication
+auth = OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+# set up the API with the authentication handler
+api = API(auth)
+
+# set up words to hear
+keywords_to_hear = ['#Facebook',
+                    '#Apple',
+                    '#Instagram'
+                    ]
+
+# instantiate the SListener object
+listen = SListener(api)
+
+# instantiate the stream object
+stream = Stream(auth, listen)
+
+# # create a engine to the database
+# engine = create_engine("sqlite:///tweets.sqlite")
+# # if the database does not exist
+# if not database_exists(engine.url):
+#     # create a new database
+#     create_database(engine.url)
+
+# begin collecting data
+while True:
+    # maintian connection unless interrupted
+    try:
+        stream.filter(track=keywords_to_hear)
+    # reconnect automantically if error arise
+    # due to unstable network connection
+    except (ProtocolError, AttributeError):
+        continue
+
